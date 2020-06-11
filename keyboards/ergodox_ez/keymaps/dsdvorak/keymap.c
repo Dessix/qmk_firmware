76ac25856aa3ba0cc3d675eadc3189ce3d046a34
Zoey <Dessix@Dessix.net>
2020-06-11 00:15:19 -0700

    Replaced left control semicolon with semicolon

--- keyboards/ergodox_ez/keymaps/dsdvorak/keymap.c
+++ keyboards/ergodox_ez/keymaps/dsdvorak/keymap.c
@@ -160,7 +160,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GRAVE,            KC_1,                 KC_2,       KC_3,         KC_4,       KC_5,    KC_BSLS,
       LT(COMBINED,KC_TAB), KC_QUOTE,             KC_COMMA,   KC_DOT,       KC_P,       KC_Y,    KC_LBRC,
       KC_ESC,              KC_A,                 KC_O,       KC_E,         KC_U,       KC_I,
-      KC_LSHIFT,           LCTL_T(KC_SCOLON),    KC_Q,       KC_J,         KC_K,       KC_X,    KC_MINUS,
+      KC_LSHIFT,           KC_SCOLON,            KC_Q,       KC_J,         KC_K,       KC_X,    KC_MINUS,
       KC_LCTL,             KC_LGUI,              KC_LALT,    KC_LEFT,      KC_RIGHT,
 
       // left thumb cluster
