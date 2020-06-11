b7d55eba69e43e65182e083497c0b6196fc66a77
Zoey <Dessix@Dessix.net>
2020-06-11 00:55:01 -0700

    Updated to use newer, more-efficient build of QMK

--- keyboards/ergodox_ez/keymaps/dsdvorak/keymap.c
+++ keyboards/ergodox_ez/keymaps/dsdvorak/keymap.c
@@ -17,6 +17,9 @@
 
 #define PERMISSIVE_HOLD
 
+#define RGBLIGHT_COLOR_LAYER_0 0x00, 0x00, 0x00
+#define RGBLIGHT_COLOR_LAYER_1 0x00, 0x00, 0x66
+
 static bool mouse_lock = false;
 
 enum custom_keycodes {
@@ -431,19 +434,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         eeconfig_init();
       }
       return false;
-      break;
     case VRSN:
       if (record->event.pressed) {
         SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
       }
       return false;
-      break;
     case RGB_SLD:
       if (record->event.pressed) {
         rgblight_mode(1);
       }
       return false;
-      break;
   }
 
   // shell macros
@@ -544,42 +544,77 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
-void led_set_user(uint8_t usb_led) {
-    if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
+// Runs just one time when the keyboard initializes.
+void keyboard_post_init_user(void) {
+#ifdef RGBLIGHT_COLOR_LAYER_0
+  rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0);
+#endif
+};
+
+// Runs whenever there is a layer state change.
+layer_state_t layer_state_set_user(layer_state_t state) {
+  ergodox_board_led_off();
+  ergodox_right_led_1_off();
+  ergodox_right_led_2_off();
+  ergodox_right_led_3_off();
+
+  uint8_t layer = get_highest_layer(state);
+  switch (layer) {
+      case 0:
+        #ifdef RGBLIGHT_COLOR_LAYER_0
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0);
+        #endif
+        break;
+      case 1:
         ergodox_right_led_1_on();
-    } else {
-        ergodox_right_led_1_off();
+        #ifdef RGBLIGHT_COLOR_LAYER_1
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_1);
+        #endif
+        break;
+      case 2:
+        ergodox_right_led_2_on();
+        #ifdef RGBLIGHT_COLOR_LAYER_2
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_2);
+        #endif
+        break;
+      case 3:
+        ergodox_right_led_3_on();
+        #ifdef RGBLIGHT_COLOR_LAYER_3
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_3);
+        #endif
+        break;
+      case 4:
+        ergodox_right_led_1_on();
+        ergodox_right_led_2_on();
+        #ifdef RGBLIGHT_COLOR_LAYER_4
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_4);
+        #endif
+        break;
+      case 5:
+        ergodox_right_led_1_on();
+        ergodox_right_led_3_on();
+        #ifdef RGBLIGHT_COLOR_LAYER_5
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_5);
+        #endif
+        break;
+      case 6:
+        ergodox_right_led_2_on();
+        ergodox_right_led_3_on();
+        #ifdef RGBLIGHT_COLOR_LAYER_6
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_6);
+        #endif
+        break;
+      case 7:
+        ergodox_right_led_1_on();
+        ergodox_right_led_2_on();
+        ergodox_right_led_3_on();
+        #ifdef RGBLIGHT_COLOR_LAYER_7
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_7);
+        #endif
+        break;
+      default:
+        break;
     }
-}
 
-void matrix_scan_user(void) {
-
-    uint8_t layer = biton32(layer_state);
-
-    ergodox_board_led_off();
-    //ergodox_right_led_2_off();
-    //ergodox_right_led_3_off();
-    switch (layer) {
-        case COMBINED:
-            //ergodox_right_led_2_on();
-            break;
-        /* case SHELL_NAV: */
-        /* case SHELL_SCREEN: */
-        case KEYNAV:
-        case KEYSEL:
-		    /* case SCREEN_NAV: */
-        /*     //ergodox_right_led_3_on(); */
-        /*     break; */
-        case MOUSE:
-            ergodox_right_led_1_on();
-            break;
-		    /* case BROWSER_CONTROL: */
-        /*     //ergodox_right_led_2_on(); */
-        /*     //ergodox_right_led_3_on(); */
-        /*     break; */
-        default:
-            ergodox_right_led_1_off();
-            break;
-    }
-
+  return state;
 };
